82292195eabc988a3ea2be8355f23642458564ff
Berke-mekatronik <73259896+Berke-mekatronik@users.noreply.github.com>
2020-11-11 21:31:53 +0300

    Update main.c

--- Sinif_Listesi/Berke_Ozturk/3_Referans/main.c
+++ Sinif_Listesi/Berke_Ozturk/3_Referans/main.c
@@ -1,1 +1,13 @@
+#include <iostream>
+using namespace std;
 
+int main(){
+    int x=10;
+    int &ref=x;
+
+    ref=20;
+    cout <<"x="<<x<<endl;
+    x=30;
+    cout<<"ref="<<ref<<endl;
+    return 0;
+}
