d5551cf25c42e710432f1fc432477042e7d76946
Huseyin YUCE <huseyin@marmara.edu.tr>
2021-01-14 16:07:33 +0300

    Template örnekleri

--- Ornekler/32_Template_3/main.cpp
+++ Ornekler/32_Template_3/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include "Dizi.h"
 
+using namespace std;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Dizi<int> intDizi(12);
+    Dizi<double> doubleDizi(12);
+
+    for (int sayac = 0; sayac < intDizi.getUzunluk(); ++sayac) {
+        intDizi[sayac] = sayac;
+        doubleDizi[sayac] = sayac + 0.5;
+    }
+
+    for (int sayac = intDizi.getUzunluk()-1; sayac >=0 ; --sayac) {
+        cout << intDizi[sayac] << "\t" << doubleDizi[sayac] << endl;
+    }
+
     return 0;
 }
