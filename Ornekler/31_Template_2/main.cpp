463b42e4122147d885b5db76dd1a99e314471acc
HYUCE <huseyin@marmara.edu.tr>
2022-01-12 20:52:29 +0300

    Update main.cpp

--- Ornekler/31_Template_2/main.cpp
+++ Ornekler/31_Template_2/main.cpp
@@ -1,19 +1,21 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
-class Para{
+
+class Para {
 private:
     int m_para;
 public:
-    Para(int para) : m_para(para){
+    Para(int para) : m_para(para) {}
 
+    // Para(int para){ m_para=para;}
+    friend bool operator>(const Para &p1, const Para &p2) {
+        return (p1.m_para > p2.m_para);
     }
-//    Para(int para){
-//    m_para=para;
-//    }
 
-friend  bool operator>(const Para &p1, const Para &p2){
-    return (p1.m_para > p2.m_para);
+    int get(){
+        return m_para;
     }
 
 };
@@ -23,11 +25,13 @@ const T& maksimum(const T& x, const T& y){
     return (x > y) ? x : y;
 }
 
+
 int main() {
     Para besLira(5);
-    Para onLira(10);
-
+    Para onLira(15);
     Para buyuk = maksimum(besLira, onLira);
 
+    cout<< "Buyugun Degeri: " << buyuk.get() << endl;
+    
     return 0;
 }
