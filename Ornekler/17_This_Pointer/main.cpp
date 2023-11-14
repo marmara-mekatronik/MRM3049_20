1959d507c6d7ea41b5632c497430e3748aea3f72
HYUCE <huseyin@marmara.edu.tr>
2023-11-14 12:08:19 +0300

    Update main.cpp

--- Ornekler/17_This_Pointer/main.cpp
+++ Ornekler/17_This_Pointer/main.cpp
@@ -1,38 +1,33 @@
-    #include <iostream>
-
+#include <iostream>
 using namespace std;
-class Kutu {
+
+class Kutu{
 public:
-    Kutu (double g=2.0, double d=2.0, double y=2.0);
-    double hacim ();
-    int karsilastir (Kutu kutu);
+    Kutu(double g=2.0, double d=2.0, double y=2.0);
+    double hacim();
+    int karsilastir(Kutu kutu);
 private:
-    double genislik;
-    double derinlik;
-    double yukseklik;
+    double genislik, derinlik, yukseklik;
 };
 Kutu::Kutu(double g, double d, double y) {
-    cout << "Yapıcı fonksiyon çağrılıyor..." << endl;
-    genislik = g;
-    derinlik = d;
-    yukseklik = y;
+    cout<< "Kurucu fonksiyon yukleniyor..." << endl;
+    this->genislik=g; derinlik=d; yukseklik=y;
 }
-
 double Kutu::hacim() {
- return genislik * derinlik * yukseklik;
+    return genislik*derinlik*yukseklik;
 }
 int Kutu::karsilastir(Kutu kutu) {
     return this->hacim() > kutu.hacim();
 }
-
 int main() {
-    Kutu Kutu1(3.3,1.2,1.5);
-    Kutu Kutu2(8.5,6.0,2.0);
-    if(Kutu1.karsilastir(Kutu2))
-    {
-        cout << "Kutu 2 daha kucuktur kutu 1 den " << endl;
-    } else {
-        cout << "Kutu 1 daha kucuktur kutu 2 den " << endl;
+    Kutu birinciKutu;
+    Kutu ikinciKutu(0.3,0.4,0.5);
+    cout << "1. Kutu Hacimi :" << birinciKutu.hacim() << endl;
+    cout << "2. Kutu Hacimi :" << ikinciKutu.hacim() << endl;
+    if(birinciKutu.karsilastir(ikinciKutu)){
+        cout<< "Kutu 2 kutu 1'den daha kucuktur" <<endl;
+    }else{
+        cout<< "Kutu 1 kutu 2'den daha kucuktur" <<endl;
     }
-
+    return 0;
 }
