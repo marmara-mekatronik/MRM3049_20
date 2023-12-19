a2766958dc974edaea29448780d31a5bd712de93
HYUCE <huseyin@marmara.edu.tr>
2023-12-19 11:32:16 +0300

    Thanks Mahmut , update main.cpp

--- Ornekler/22_Overloading_Asiri_Yukleme_3/main.cpp
+++ Ornekler/22_Overloading_Asiri_Yukleme_3/main.cpp
@@ -1,61 +1,48 @@
 #include <iostream>
-
 using namespace std;
 class Kutu{
+private:
+    double uzunluk,genislik,yukseklik,hacim;
 public:
     double getHacim();
-    void setUzunluk(double uzn);
-    void setGenislik(double gen);
-    void setYukseklik(double yuk);
-    Kutu operator+(const Kutu& b){
+    void setOzellikler(double uzn,double gen,double yuk);
+
+    Kutu operator+(Kutu& b){
         Kutu kutu1;
-        kutu1.uzunluk=this->uzunluk + b.uzunluk;
-        kutu1.genislik=this->genislik + b.genislik;
-        kutu1.yukseklik= this->yukseklik + b.yukseklik;
+        kutu1.hacim= this->getHacim()+b.getHacim();
         return kutu1;
     }
-
-private:
-    double uzunluk, genislik, yukseklik;
 };
 
 double Kutu::getHacim() {
-    return uzunluk*genislik*yukseklik;
+    return hacim;
 }
-void Kutu::setYukseklik(double yuk) {
-    yukseklik=yuk;
-}
-void Kutu::setGenislik(double gen) {
-    genislik=gen;
-}
-void Kutu::setUzunluk(double uzn) {
+void Kutu::setOzellikler(double uzn,double gen,double yuk) {
     uzunluk=uzn;
+    genislik=gen;
+    yukseklik=yuk;
+    hacim=uzn*gen*yuk;
 }
 
-int main() {
-    Kutu kutu1;
-    Kutu kutu2;
-    Kutu kutu3;
+int main(){
+    Kutu k1;
+    Kutu k2;
+    Kutu k3;
+    Kutu k4;
+    double hacim{};
 
-   double hacim=0.00;
-
-    kutu1.setUzunluk(1.10);
-    kutu1.setGenislik(1.20);
-    kutu1.setYukseklik(1.30);
-
-    hacim = kutu1.getHacim();
+    k1.setOzellikler(1.00,1.00,1.00);
+    hacim = k1.getHacim();
     cout << "Kutu 1'in hacmi : " << hacim << endl;
-
-    kutu2.setUzunluk(2.10);
-    kutu2.setGenislik(2.20);
-    kutu2.setYukseklik(2.30);
-
-    hacim = kutu2.getHacim();
+    k2.setOzellikler(2.00,2.00,2.00);
+    hacim = k2.getHacim();
     cout << "Kutu 2'in hacmi : " << hacim << endl;
-
-    kutu3 =  kutu1 + kutu2;
-    hacim = kutu3.getHacim();
+    k3 =  k1 + k2;
+    hacim = k3.getHacim();
     cout << "Kutu 3'in hacmi : " << hacim << endl;
 
+    k4 = k3+k1+k2;
+    hacim = k4.getHacim();
+    cout << "Kutu 4'in hacmi : " << hacim << endl;
     return 0;
 }
