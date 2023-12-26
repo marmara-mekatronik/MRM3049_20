51de908c464b265e1e0130bbd3bc4983e5d75ddb
HYUCE <huseyin@marmara.edu.tr>
2023-12-26 10:53:17 +0300

    Update main.cpp

--- Ornekler/25_Polymorphism_4/main.cpp
+++ Ornekler/25_Polymorphism_4/main.cpp
@@ -1,45 +1,47 @@
 #include <iostream>
-#include <iomanip>
 
 using namespace std;
-class Sensor{
+
+class Sensor {
 public:
-    virtual int veri(int deger)=0;
+    virtual int veri(int deger) = 0;
+    virtual ~Sensor() {} // Sanal yıkıcı eklenmiştir
 };
 
-class Sensor1:public Sensor{
+class Sensor1 : public Sensor {
 public:
-    int veri(int deger)override{
+    int veri(int deger) override {
         return deger;
-    };
+    }
 };
 
-class Sensor2:public Sensor{
+class Sensor2 : public Sensor {
 public:
-    int veri(int deger) override{
+    int veri(int deger) override {
         return deger;
-    };
+    }
 };
 
-class Sensor3:public Sensor{
+class Sensor3 : public Sensor {
 public:
-    int veri(int deger)override{
+    int veri(int deger) override {
         return deger;
-    };
+    }
 };
-
-
-
 
 int main() {
-    Sensor *ptrSensor[3];
-    ptrSensor[0]=new Sensor1();
-    ptrSensor[1]=new Sensor2();
-    ptrSensor[2]=new Sensor3();
+    Sensor* ptrSensor[3];
+    ptrSensor[0] = new Sensor1();
+    ptrSensor[1] = new Sensor2();
+    ptrSensor[2] = new Sensor3();
 
     for (int i = 0; i < 3; ++i) {
-        cout << ptrSensor[i]->veri((i+1)*20)<<endl;
+        cout << ptrSensor[i]->veri((i + 1) * 20) << endl;
+    }
 
+    // Bellek sızıntısını önlemek için delete kullanılmalı
+    for (int i = 0; i < 3; ++i) {
+        delete ptrSensor[i];
     }
 
     return 0;
