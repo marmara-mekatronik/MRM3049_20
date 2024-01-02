4f9618f057815a8cddab93f3f9d61991757b289e
HYUCE <huseyin@marmara.edu.tr>
2024-01-02 10:58:37 +0300

    Update main.cpp

--- Ornekler/22_Overloading_Asiri_Yukleme_4/main.cpp
+++ Ornekler/22_Overloading_Asiri_Yukleme_4/main.cpp
@@ -1,38 +1,64 @@
 #include <iostream>
 #include <iomanip>
+
+#ifdef _WIN32
+
 #include <windows.h>
+
+#endif
+#ifdef __unix__
+#include <unistd.h>
+#endif
+
 using namespace std;
-class Zaman{
+
+class Zaman {
 public:
     void gosterZaman();
-    Zaman(){
-        saat=0; dakika=0;
+
+    Zaman() {
+        saat = 0;
+        dakika = 0;
     }
-    Zaman(int s, int d){
-        saat=s; dakika=d;
+
+    Zaman(int s, int d) {
+        saat = s;
+        dakika = d;
     }
-    Zaman operator++(){
-      ++dakika;
-      if(dakika>=60){
-          ++saat;
-          dakika-=60;
-      }
-        return Zaman(saat,dakika);
+
+    Zaman operator++() {
+        ++dakika;
+        if (dakika >= 60) {
+            ++saat;
+            dakika -= 60;
+        }
+        return Zaman(saat, dakika);
     }
+
 private:
     int saat, dakika;
 };
+
 void Zaman::gosterZaman() {
-    cout << setw(2) << setfill('0')<< saat << " : " << setw(2)
-    << setfill('0') << dakika <<  "\r";
+    cout << setw(2) << setfill('0') << saat << " : " << setw(2)
+         << setfill('0') << dakika << "\r";
 }
+
 int main() {
-    Zaman Z1(01,59);
+    Zaman Z1(01, 59);
 
     for (int i = 0; i < 120; ++i) {
         ++Z1;
         Z1.gosterZaman();
-        Sleep(100);
+#ifdef _WIN32
+        // Windows özel kodu
+        Sleep(100); // 1 saniye bekletme
+#endif
+
+#ifdef __unix__
+        // UNIX özel kodu
+    sleep(1); // 1 saniye bekletme
+#endif
     }
     return 0;
 }
