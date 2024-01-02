aab2d5bdc635ad1b3e6460019e8fd2533beb8437
HYUCE <huseyin@marmara.edu.tr>
2024-01-02 10:55:16 +0300

    Update main.cpp

--- Ornekler/22_Overloading_Asiri_Yukleme_4/main.cpp
+++ Ornekler/22_Overloading_Asiri_Yukleme_4/main.cpp
@@ -1,61 +1,38 @@
 #include <iostream>
 #include <iomanip>
+#include <windows.h>
 using namespace std;
 class Zaman{
 public:
     void gosterZaman();
     Zaman(){
-        saat=0;
-        dakika=0;
+        saat=0; dakika=0;
     }
     Zaman(int s, int d){
-        saat = s;
-        dakika = d;
+        saat=s; dakika=d;
     }
     Zaman operator++(){
-        ++dakika;
-        if (dakika>=60){
-            ++saat;
-            dakika-=60;
-        }
-        return Zaman(saat, dakika);
+      ++dakika;
+      if(dakika>=60){
+          ++saat;
+          dakika-=60;
+      }
+        return Zaman(saat,dakika);
     }
-    Zaman operator++(int){
-        Zaman Z(saat,dakika);
-        ++dakika;
-        if (dakika>=60){
-            ++saat;
-            dakika-=60;
-        }
-        return Z;
-    }
-
 private:
-    int saat;
-    int dakika;
+    int saat, dakika;
 };
-
 void Zaman::gosterZaman() {
-    cout << setw(2) << setfill('0')<< saat << " : " << setw(2) << setfill('0') << dakika << endl;
-// setw(2) << setfill('0') ifadesi 00:00 daki dijiti gösterebilmek için yazıldı
+    cout << setw(2) << setfill('0')<< saat << " : " << setw(2)
+    << setfill('0') << dakika <<  "\r";
 }
-
 int main() {
     Zaman Z1(01,59);
-    Z1.gosterZaman();
-    Zaman Z2(10,40);
-    Z2.gosterZaman();
-    ++Z1;
-    Z1.gosterZaman();
-    ++Z1;
-    Z1.gosterZaman();
 
-    cout<<"_____________________" << endl;
-
-    Z2++;
-    Z2.gosterZaman();
-    Z2++;
-    Z2.gosterZaman();
-
+    for (int i = 0; i < 120; ++i) {
+        ++Z1;
+        Z1.gosterZaman();
+        Sleep(100);
+    }
     return 0;
 }
